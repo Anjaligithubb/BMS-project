12de3a6908c85154f3cd42738bd6611262ac4670
User <User@DESKTOP-UE3G1D7>
2024-11-14 17:39:12 +0530

    initial commit

---
+++ spring-boot-rest-data-student-project/src/main/java/com/demo/spring_boot_rest_data_student_project/service/StudentServiceImpl.java
@@ -0,0 +1,42 @@
+
+package com.demo.spring_boot_rest_data_student_project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.spring_boot_rest_data_student_project.dao.StudentDao;
+import com.demo.spring_boot_rest_data_student_project.entity.StudentEntity;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	@Autowired
+	StudentDao studentDao;
+	
+	@Override
+	public List<StudentEntity> fetchAllStudents() {
+		return studentDao.findAll();
+	}
+
+	@Override
+	public StudentEntity fetchAStudent(int studId) {
+		return studentDao.findById(studId).get();
+	}
+
+	@Override
+	public StudentEntity addStudent(StudentEntity newStud) {
+		return studentDao.saveAndFlush(newStud);
+	}
+
+	@Override
+	public StudentEntity updateStudent(StudentEntity updateStud) {
+		return studentDao.saveAndFlush(updateStud);
+	}
+
+	@Override
+	public void deleteStudent(int studId) {
+		studentDao.deleteById(studId);
+	}
+}
